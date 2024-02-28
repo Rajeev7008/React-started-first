f470562db6ade7d4e6bdef95e8c52fdaf5942fcd
Rajeev <112148973+Rajeev7008@users.noreply.github.com>
2024-02-28 16:33:04 +0000

    counterProject

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+
+
+function App() {
+  return (
+    <h1>hello React | Rajeev Kumar</h1>
+   
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  let [counter, setCount] = useState(0)
+  const AddValue = () => {
+    if(counter == 20){
+
+    }
+    else{
+      setCount(counter+1)
+    }
+  }
+  const RemoveValue = () => {
+    if(counter == 0){
+
+    }
+    else{
+      setCount(counter-1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Hello React Start count</h1>
+      <h1>CountValue: {counter}</h1>
+      <button onClick={AddValue}>Add Value{counter}</button><br />
+      <button onClick={RemoveValue}>Remove Value{counter}</button>
+    </>
+  )
+}
+
+export default App
